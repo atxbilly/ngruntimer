f72791dfd8930e7a0785b9b7fd13d388a6dbe3ca
Brad Harris <bradharris512@gmail.com>
2018-07-29 21:20:20 -0500

    fix server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/poeruntimer'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/poeruntimer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
